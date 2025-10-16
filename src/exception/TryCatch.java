a3c074902a7c5dd72d870f651f8502411b35bf11
stdiodh <playlistdh@gmail.com>
2025-10-16 16:11:16 +0900

    checkAge 예제 추가

--- src/exception/TryCatch.java
+++ src/exception/TryCatch.java
@@ -1,13 +1,25 @@
 package exception;
 
+import console.view.InputView;
+
 public class TryCatch {
     public static void main(String[] args){
-        try {
-            // ArithmeticException 발생지점
-            int result = 10 / 0;
-        } catch(ArithmeticException e) {
-            // ArithmeticException 이 발생했을 때 실행될 코드
-            System.out.println("ArithmeticException 예외가 발생했습니다.");
+        InputView inputView = new InputView();
+
+        String age = inputView.getUserInput();
+        int stringToIntAge = toInt(age);
+
+        System.out.println(checkAge(stringToIntAge));
+    }
+
+    public static boolean checkAge(int age) {
+        if (age < 19) {
+            throw new IllegalArgumentException("미성년자는 접근할 수 없습니다.");
         }
+        return true;
+    }
+
+    public static int toInt(String st){
+        return Integer.parseInt(st);
     }
 }
