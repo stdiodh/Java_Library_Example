5e408972abc8968be83918254980dba84d701010
stdiodh <playlistdh@gmail.com>
2025-10-17 18:49:09 +0900

    example class

---
+++ src/exClass/ExClass.java
@@ -0,0 +1,11 @@
+package exClass;
+
+public class ExClass {
+    public static void main(String[] args){
+        Person exPerson = new Person("이순신", 30, 188.0F);
+
+        System.out.println(exPerson.name);
+        System.out.println(exPerson.age);
+        System.out.println(exPerson.height);
+    }
+}
---
+++ src/exClass/Person.java
@@ -0,0 +1,13 @@
+package exClass;
+
+public class Person {
+    String name;
+    int age;
+    float height;
+
+    public Person(String pName, int pAge, float pHeight){
+        name = pName;
+        age = pAge;
+        height = pHeight;
+    }
+}
